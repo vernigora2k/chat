b1ed35ce502dd80f0f5c5bb7422b6c260df61589
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-04-03 23:17:01 +0300

    v1.0.3temp aded socket.emit in index.js

--- js/controller.js
+++ js/controller.js
@@ -11,3 +11,9 @@ socket.on('message').addEventListener () {
         console.log('');
     }
 }
+
+// io.on('connection', function(socket){
+//     socket.on('chat message', function(msg){
+//       console.log('message: ' + msg);
+//     });
+//   });
--- js/index.js
+++ js/index.js
@@ -2,5 +2,13 @@ import {} from './controller.js';
 import {} from './chatView.js';
 import {socket} from './client.js';
  
-
+$(function () {
+    var socket = io();
+    $('form').submit(function(e){
+      e.preventDefault(); 
+      socket.emit('chat message', $('.messageInput').val());
+      $('.messageInput').val('');
+      return false;
+    });
+  });
  

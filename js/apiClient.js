6ae5c9652ddd4f6298bad9accba7942eb6a05970
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-04-18 22:09:57 +0300

    v1.3.0temp working on createUser

--- js/apiClient.js
+++ js/apiClient.js
@@ -1,18 +1,45 @@
 import { URL } from "./config.js";
 
-// const params = 'username=John';
-
-export function apiRequest(url, method, params) {
-    const config = {
-        method: `${method}`,
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    };
-   
-    fetch(url + '/api/user?' + params, config)
+function apiRequest(url, config) {
+    fetch(url, config)
         .then(dataFromServer => dataFromServer.json())
         .catch(err => console.log("Ошибка!", err))
 }
 
+export function createUser(username, password) {
+    const url = URL + '/api/user'
+    const payload = {
+        username: username,
+        password: password,
+    }
+    const config = {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    }
+    new Promise((resolve, reject) => {
+        const data = apiRequest(url, config)
+        resolve(data)
+    })
+        .then(data => console.log(data)) 
+        .catch(error => alert(error))
+}
+
+
+
+// export function apiRequest(url, method, params) {
+//     const config = {
+//         method: `${method}`,
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//     };
+   
+//     fetch(url + '/api/user?' + params, config)
+//         .then(dataFromServer => dataFromServer.json())
+//         .catch(err => console.log("Ошибка!", err))
+// }

d74a7a35b2372d9ba791a1d767ec2443da53f630
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-04-19 21:48:01 +0300

    v1.3.3temp autorization in controller.js on working

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,6 @@
 import {socket} from './client.js';
 import {createNewMessage} from './chatView.js'
+import {apiRequest} from './apiClient.js'
 
 export function sendMessage(msg) {
     socket.emit('message', msg);
@@ -8,3 +9,24 @@ export function sendMessage(msg) {
 socket.on('message', function(msg){
     createNewMessage(msg, true);
 });
+
+export function autorization(username, password) {
+    const url = 'api/user/auth'
+    const payload = {
+        username: username,
+        password: password,
+    }
+    const config = {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    }
+
+    apiRequest(ulr, config)
+        .then()
+        .catch(error => alert(error));
+        )
+}

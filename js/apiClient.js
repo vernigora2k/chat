6f53d5b4ed7d0bfec70a22469a749e1787e9cba4
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-04-19 20:32:20 +0300

    v1.3.1stable added method createUser in apiClient

--- js/apiClient.js
+++ js/apiClient.js
@@ -1,13 +1,13 @@
 import { URL } from "./config.js";
 
 function apiRequest(url, config) {
-    fetch(url, config)
+    fetch(URL + url, config)
         .then(dataFromServer => dataFromServer.json())
         .catch(err => console.log("Ошибка!", err))
 }
 
 export function createUser(username, password) {
-    const url = URL + '/api/user'
+    const url = '/api/user'
     const payload = {
         username: username,
         password: password,
@@ -20,26 +20,7 @@ export function createUser(username, password) {
         },
         body: JSON.stringify(payload)
     }
-    new Promise((resolve, reject) => {
-        const data = apiRequest(url, config)
-        resolve(data)
-    })
+    apiRequest(url, config)
         .then(data => console.log(data)) 
         .catch(error => alert(error))
 }
-
-
-
-// export function apiRequest(url, method, params) {
-//     const config = {
-//         method: `${method}`,
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//     };
-   
-//     fetch(url + '/api/user?' + params, config)
-//         .then(dataFromServer => dataFromServer.json())
-//         .catch(err => console.log("Ошибка!", err))
-// }
